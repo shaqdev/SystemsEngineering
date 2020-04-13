2908b15773ada7e51c2dfa545104ac7f26d8a8cc
shaqdev <shaqkhaleel@gmail.com>
2020-04-13 08:00:31 +0530

    Added shell sort in .../chap_3/practice.c

--- Programming/C_C++/The_C_ProgrammingL/chap_3/practice.c
+++ Programming/C_C++/The_C_ProgrammingL/chap_3/practice.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <ctype.h> // to include isspace() and isdigit()
+
+// Switch case to count no. of digits/white space/other chars
+void count_char();
+
+// atoi for converting string to its numeric equivalent.
+// It should copy with leading whitespaces and + or - signs
+int atoi();
+
+// shell sort
+void shellsort(int[], int);
 
 int main()
 {
+	int i;
 	int a = 10;
 	{
 		a = 20;
@@ -11,6 +23,33 @@ int main()
 	printf("a*a : %d\n",a*a);
 
 	// Switch case to count no. of digits/white space/other chars
+	count_char();
+
+	// atoi for converting string to its numeric equivalent.
+	int atoi_ret = atoi();
+	printf("atoi version 2, atoi_ret : %d\n", atoi_ret);
+
+	//shell sort
+	int shell_arr[8] = {5,6,4,2,8,1,7,9};
+
+	printf("Before shell sort :\t");
+	for (i = 0; i < 8; ++i)
+		printf("%d\t",shell_arr[i]);
+	printf("\n");
+
+	shellsort(shell_arr, 8);
+	
+	printf("After shell sort  :\t");
+	for (i = 0; i < 8; ++i)
+		printf("%d\t",shell_arr[i]);
+	printf("\n");
+
+
+return 0;
+}
+
+void count_char()
+{
 	int digits = 0, white = 0, other = 0;
 	char ch;
 
@@ -31,6 +70,50 @@ int main()
 		}
 	}
 	printf("digits : %d\nwhite : %d\nother : %d\n",digits, white, other);
+}
 
-return 0;
+//atoi : Converts string to integer; version 2
+int atoi()
+{
+	int i, n, sign;
+	char s[] = "  -12365";
+
+	//leave the leading white spaces
+	for (i = 0; isspace(s[i]); ++i);
+
+	//store the sign
+	sign = (s[i] == '-') ? -1 : 1;
+
+	//if sign character exists in string, increment the i
+	if (s[i] == '+' || s[i] == '-')
+		++i;
+
+	for (n = 0; isdigit(s[i]); ++i)
+		n = n * 10 + (s[i] - '0');
+
+	return sign * n;
+}
+
+// shell sort 
+/*
+	first for : iterates through gap
+	second for : iterates through elements
+	third for : sorts and iterate backwards till first element
+*/
+void shellsort(int v[], int n)
+{
+	int gap, i, j, temp;
+
+	for (gap = n/2; gap > 0; gap /= 2)
+	{
+		for (i = gap; i < n; ++i)
+		{
+			for (j = i-gap; j >= 0 && v[j] > v[j+gap]; j -= gap)
+			{
+				temp = v[j+gap];
+				v[j+gap] = v[j];
+				v[j] = temp;
+			}
+		}
+	}
 }
