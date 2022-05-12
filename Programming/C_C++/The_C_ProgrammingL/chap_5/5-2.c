ea524d477cc5711cda99f31eb6b4177aba4c5294
shaqdev <shaqkhaleel@gmail.com>
2022-05-12 08:16:23 +0530

    update Programming/C_C++/The_C_ProgrammingL/chap_5 from PLinux

--- Programming/C_C++/The_C_ProgrammingL/chap_5/5-2.c
+++ Programming/C_C++/The_C_ProgrammingL/chap_5/5-2.c
@@ -1,26 +1,69 @@
+/*Write getfloat, the floating-point analog of getint. 
+  What type does getfloat return as its function value? */
+
+
 #include <stdio.h>
+#include <ctype.h>
+
+#define BUF_LEN 10
+float input_buf[BUF_LEN] = {0.0};
+int cnt = 0;
+char getch(){
+  char ch;
+  if (cnt > 0)
+    ch = input_buf[cnt];
+  else
+    ch = getchar();
+}
+
+void ungetch(char ch) {
+  if (cnt >= BUF_LEN) {
+    printf("Input buffer is full ! Unable to insert !");
+    return;
+  }
+  else {
+    input_buf[cnt] = ch;
+    cnt++;
+  }
+}
 
 #define MAX_VAL 10
 
-bool getfloat(float *ip) {    // complete getfloat
+int getfloat(float *ip) {    // complete getfloat
   char c;
   short sign;
+  *ip = 0.0;
   
-  while ((c = getch()) != ' ')
+  while ((c = getch()) == ' ')
   ;
 
-  if(!isdigit(c) && c != '+' && c != '-' && c != EOF)
+  if(!isdigit(c) && c != '+' && c != '-' && c != EOF && c != '.')
     return 0;
 
-  sign = (c == '-1') ? -1 : 1;
+  sign = (c == '-') ? -1 : 1;
   if(c == '+' || c == '-')
     c = getch();
 
-  for(*ip = 0; isdigit(c); c = getch())
-      *ip = 10 * *ip + (c - '0');
+  float dec;
+  float fc = 0.0;
+  for(dec = 0.0; isdigit(c); c = getch())
+      dec = 10 * dec + (c - '0');
 
-  if(c == '.')
+  if(c == '.') {
+    c = getch();
+
+    for (int n = 1; isdigit(c); c = getch(), ++n) {
+      fc = fc + (float)(c - '0')/(float)(n*10);
+    }
+  }
+
+  *ip = sign * (dec + fc);
+
+
+  //if (c != EOF)
+  //  ungetch(c);
   
+  return c;
 }
 
 int main(){
@@ -29,12 +72,12 @@ int main(){
   int c;
 
   for(int i = 0; i < MAX_VAL; ++i)
-    printf("%d\n",ft[i]);
+    printf("%f\n",ft[i]);
 
   for(c = 0; c < MAX_VAL && getfloat(&ft[c]) != EOF; ++c)
   ;
 
   for(int i = 0; i < MAX_VAL; ++i)
-    printf("%d\n",ft[i]);
+    printf("%f\n",ft[i]);
   return 0;
 }
